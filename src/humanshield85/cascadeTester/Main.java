05165ecb37dc7eb693b419a2c03e63de4b0378d4
humanshield85 <rachidboudjelida@gmail.com>
2018-09-24 13:57:52 +0100

    initial commit

---
+++ src/humanshield85/cascadeTester/Main.java
@@ -0,0 +1,11 @@
+package humanshield85.cascadeTester;
+
+import humanshield85.cascadeTester.ui.MainUi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
